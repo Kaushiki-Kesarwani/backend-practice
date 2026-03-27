480076b12ca82949baa870c1f7d040a52843a07a
mahak <kaushikikes@gmail.com>
2026-03-27 16:39:06 +0530

    taking various input and output

---
+++ userInput.js
@@ -0,0 +1,22 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+input:process.stdin,
+output:process.stdout,
+});
+
+rl.question("What is your name?",name=>{
+    console.log(`My name is ${name}.`)
+    rl.question("What course are you studying?",course=>{
+        console.log(` ${course} (Bachelor's of computer applications).`)
+        rl.question("How old are you?",age=>{
+            console.log(`I am ${age} years.`)
+            rl.question("What are the skills do you have?",skills=>{
+                console.log(` I am good in these skills ${skills}.`)
+                rl.question("Which is your strongest coding language?",code=>{
+                    console.log(`${code}.`)
+                })
+            })
+        })
+    })
+})
